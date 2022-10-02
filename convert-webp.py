2b31cbf2f7728c79bc9c31ec3b930d9d96c23a99
Jefferson X. Espinoza <jefferx6@gmail.com>
2022-10-02 01:22:26 -0500

    First commit

---
+++ convert-webp.py
@@ -0,0 +1,84 @@
+import os
+import time
+from PIL import Image
+
+print('\n========================================')
+print('Develope by jeffersonespinoza.com \n')
+print('30 sep 2022')
+print('==========================================\n')
+
+folder = 'images'
+
+star = time.time()
+
+## Convert png to webp
+## Does not save transparency
+## Do not use with images that have transparent backgrounds 
+## because it distorts the image.
+
+
+# Find jpg files in folder
+for root, dirs, files in os.walk(folder):
+    for file in files:
+        # Fin file with you extention 
+        if file.endswith(( '.png' )):
+            file = os.path.join(root, file)
+            # Extention to convert
+            newFile = file.replace( '.png', '.webp' )
+
+            # Convert to webp
+            im = Image.open(file).convert('RGB')
+            im.save(newFile,'webp')
+
+            ## Open file
+            im = Image.open(file)
+            width, heigth = im.size
+            
+            ## Resize you image
+            ## Use of 1 - 6 for resize
+            ## For use uncomment next line 
+
+            ## Change quality image -> quality = (1/100)
+            ## Reduce quality uncomment next line
+
+            # im.save(newFile, quality =  60)
+
+            print(im.size)
+            print(newFile)  
+
+# Find jpg files in folder
+for root, dirs, files in os.walk(folder):
+    for file in files:
+        # Fin file with you extention 
+        if file.endswith(( '.jpg' )):
+            file = os.path.join(root, file)
+            # Extention to convert
+            newFile = file.replace( '.jpg', '.webp' )
+
+            # Convert to webp
+            im = Image.open(file).convert('RGB')
+            im.save(newFile,'webp')
+
+            ## Open file
+            im = Image.open(newFile)
+            width, height = im.size
+            
+            ## Resize you image
+            ## Use of 1 - 6 for resize
+            ## Uncomment next line 
+
+            #im = im.resize((width//1, heigth//1))
+
+            ## Change quality image -> quality = (1/100)
+            ## For use uncomment next line
+
+            # im.save(newFile, quality =  60)
+
+            print(im.size)
+            print(newFile)        
+
+
+end = time.time()
+
+print("\n  Work end ! time execution: ")
+print(end-star)
